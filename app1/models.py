79331d36aa73a8951c626f6ec9c7ff5d5631b7ff
Maguilera85 <drianketing@gmail.com>
2021-01-11 12:24:23 -0300

    Creacion de modelos y conexion con postgres OK

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+
+class SensoresTemp(models.Model):
+    ''' tabla para sensores de temperatura'''
+
+    nombre_sensor = models.CharField(max_length=20)
+    fecha = models.DateField()
+
+class SensoresApertura(models.Model):
+    ''' tabla para sensores de apertura '''
+
+    nombre_sensor = models.CharField(max_length=20)
+    fecha = models.DateField()
+
+class SensoresGas(models.Model):
+    ''' tabla para sensores de gasolina '''
+
+    nombre_sensor = models.CharField(max_length=20)
+    fecha = models.DateField() 
+
+class CodigosAcceso(models.Model):
+    ''' Códigos numéricos ingresados por un usuario que intenta abrir una puerta de acceso '''
+
+    pin = models.IntegerField()
--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,3 @@
 from django.shortcuts import render
-
+from .models import SensoresApertura, SensoresGas, SensoresTemp, CodigosAcceso
 # Create your views here.
